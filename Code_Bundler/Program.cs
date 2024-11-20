7c6df97d9ea9c83ee8032a9a898464481cb14783
y0533121238@gmail.com <y0533121238@gmail.com>
2024-11-20 17:38:23 +0200

    create basic command

--- Code_Bundler/Program.cs
+++ Code_Bundler/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+
+//fib bundle --output "======/bundleFile.txt" 
+
+var bundleOption = new Option<FileInfo>("--output", "file path and name");
+
+var bundleCommand = new Command("bundle", "Bundle code files for single file");
+bundleCommand.AddOption(bundleOption);
+bundleCommand.SetHandler((output) => {
+    try
+    {
+        File.Create(output.FullName);
+        Console.WriteLine("file was created");
+    }
+    catch {
+        Console.WriteLine("Error: file path is invalid");
+    }
+}, bundleOption);
+
+var rootCommand = new RootCommand("Root command for file bundler CLI");
+rootCommand.AddCommand(bundleCommand);
+rootCommand.InvokeAsync(args);
